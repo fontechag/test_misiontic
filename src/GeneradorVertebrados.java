764eed4cb075177035d20f1e7e3bdddbf4f11042
germanf <germanf@uninorte.edu.co>
2022-09-14 16:04:17 -0500

    Se eliminó "Tiburon" de la liste de vertebrados

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,9 +1,9 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
-        Vertebrado[] miVertebrados = new Vertebrado[4];
+        Vertebrado[] miVertebrados = new Vertebrado[3];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        miVertebrados[2] = new Vertebrado("Perro", "Pez"); 
         
         return miVertebrados;
     }
